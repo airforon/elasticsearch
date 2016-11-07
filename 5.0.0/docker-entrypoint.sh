a117a3f50a516b62e910ccb63379e2cd51f4dbf4
FoxBoxsnet <naokiaoyama2009@gmail.com>
2016-11-07 11:21:26 +0900

    fix

--- 5.0.0/docker-entrypoint.sh
+++ 5.0.0/docker-entrypoint.sh
@@ -22,14 +22,11 @@ fi
 echo "bootstrap.memory_lock: true"                          >   $CONFIG_FILE
 echo "indices.fielddata.cache.size: 75%"                    >>  $CONFIG_FILE
 echo "network.host: _eth0:ipv4_"                            >>  $CONFIG_FILE
-echo "discovery.zen.ping.unicast.hosts: "$CLUSTER_HOST""    >>  $CONFIG_FILE
-
-
 
 
 if [ ! -n "$CLUSTER_HOST" ];then
 	echo "Discovery Zen Ping Unicast Hosts"
-	echo "discovery.zen.ping.unicast.hosts: "$CLUSTER_HOST"" >>  $CONFIG_FILE
+	echo "discovery.zen.ping.unicast.hosts: "${CLUSTER_HOST:-"["localhost","127.0.0.1"]"}"" >>  $CONFIG_FILE
 fi
 # =============================================================================
 if [ ! -n "$CLUSTER_NAME" ];then
